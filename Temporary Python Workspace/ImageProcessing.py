af311e353be874976556457a0e6cc51379157699
Andrew McDonald <mcdonalda1993@gmail.com>
2014-01-12 03:30:15 +0000

    Changed import cv2.py into Snippets.py

--- Temporary Python Workspace/ImageProcessing.py
+++ Temporary Python Workspace/ImageProcessing.py
@@ -1,49 +1,20 @@
-from PIL import Image, ImageFilter, ImageOps
+import cv2 as cv
+# import pyopencv as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
 
-def pixel(i):
-	if(i<=20):
-		print "lowers"
-		return 0
-	else:
-		print "highers"
-		return 256
+img = cv.imread('Testface.jpg',0)
+edges = cv.Canny(img,100,200)
 
-
-im = Image.open("Testface.jpg")
-im = im.filter(ImageFilter.ModeFilter(8))
-im = im.filter(ImageFilter.GaussianBlur(2))
-# im = im.point(lambda i: i*3)
-# im = im.filter(ImageFilter.GaussianBlur(3))
-# im = im.filter(ImageFilter.UnsharpMask(150,2,30))
-# im = im.filter(ImageFilter.SMOOTH_MORE)
-im = im.filter(ImageFilter.FIND_EDGES)
-# im = im.filter(ImageFilter.SMOOTH_MORE)
-# im = im.filter(ImageFilter.GaussianBlur(2))
-
-# im = im.filter(ImageFilter.SHARPEN)
-im = ImageOps.grayscale(im)
-im = im.point(lambda i: i*3)
-# im = im.filter(ImageFilter.Kernel( (3,3), (1,1,1,1,0,1,1,1,1) ))
-# im = im.filter(ImageFilter.Kernel( (5,5), (1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1) ))
-# im = im.filter(ImageFilter.Kernel( (5,5), (1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1) ))
-# im = im.point(lambda i: i*2)
-# im = im.filter(ImageFilter.Kernel( (5,5), (1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1) ))
-# im = im.filter(ImageFilter.FIND_EDGES)
-# im = im.filter(ImageFilter.GaussianBlur(2))
-# im = im.filter(ImageFilter.SHARPEN)
-# im = im.filter(ImageFilter.UnsharpMask(150,2,30))
-
-#im = im.
-#im = im.point(pixel)
-im.save("convert2.jpg")
-
-#.filter(ImageFilter.ModeFilter(6))
-#.filter(ImageFilter.FIND_EDGES)
-#.filter(ImageFilter.Kernel( (3,3), (2,2,2,2,6,2,2,2,2) ))
-#ImageFilter.GaussianBlur()
-
-# 1,1,1,1,1,
-# 1,1,1,1,1,
-# 1,1,0,1,1,
-# 1,1,1,1,1,
-# 1,1,1,1,1
+print img
+print edges
+i=0
+# for line in edges:
+# 	i+=1
+# 	sys.stdout.write(str(i) + ": ")
+# 	sys.stdout.flush()
+# 	for pixel in line:
+# 		sys.stdout.write(str(pixel) + ", ")
+# 		sys.stdout.flush()
+# 	print " "
