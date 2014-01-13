b0737edf4af2c966f8b5751322d79dc9621b447c
Andrew McDonald <mcdonalda1993@gmail.com>
2014-01-13 23:06:25 +0000

    Created test image of circle and fixed a bug that was found through test image. Also created checkDirection function for each pixel query

--- Temporary Python Workspace/ImageProcessing.py
+++ Temporary Python Workspace/ImageProcessing.py
@@ -12,19 +12,21 @@ def createInstructions(image, colour=255):
 	set_of_instructions = []
 	x=0
 	for line in image:
-		x+=1
+		
 		y=0
 		# sys.stdout.write(str(i) + ": ")
 		# sys.stdout.flush()
 		for pixel in line:
-			y+=1
+			
 			if ((x,y) not in pixels_visited):
 				pixels_visited[(x,y)] = True
-				if(pixel==colour):
+				if(pixel>=colour):
 					instructions = [(x,y), 0]
 					pen_state = 0
 					pen_state = processLine(x, y, instructions, pen_state, pixels_visited, image, colour)
 					set_of_instructions.append(instructions)
+			y+=1
+		x+=1
 	return set_of_instructions
 	
 def processLine(x, y, instructions, pen_state, pixels_visited, image, colour):
@@ -34,138 +36,91 @@ def processLine(x, y, instructions, pen_state, pixels_visited, image, colour):
 	# X #
 	# o #
 	# # #
-	if(((x-1,y) not in pixels_visited) and image[x-1][y]==colour):
-		if(pState==1):
-			instructions.append((x,y))
-			instructions.append(0)
-			pState = 0
-		instructions.append((x-1, y))
-		pixels_visited[(x-1, y)] = True
-		pState = processLine(x-1, y, instructions, pState, pixels_visited, image, colour)
-	pixels_visited[(x-1, y)] = True
+	pState = checkDirection(x, y, -1, 0, instructions, pState, pixels_visited, image, colour)
 	
 	# # X
 	# o #
 	# # #
-	if(((x-1,y+1) not in pixels_visited) and image[x-1][y+1]==colour):
-		if(pState==1):
-			instructions.append((x,y))
-			instructions.append(0)
-			pState = 0
-		instructions.append((x-1, y+1))
-		pixels_visited[(x-1, y+1)] = True
-		pState = processLine(x-1, y+1, instructions, pState, pixels_visited, image, colour)
-	pixels_visited[(x-1, y+1)] = True
+	pState = checkDirection(x, y, -1, 1, instructions, pState, pixels_visited, image, colour)
+
 
 	# # #
 	# o X
 	# # #
-	if(((x,y+1) not in pixels_visited) and image[x][y+1]==colour):
-		if(pState==1):
-			instructions.append((x,y))
-			instructions.append(0)
-			pState = 0
-		instructions.append((x, y+1))
-		pixels_visited[(x, y+1)] = True
-		pState = processLine(x, y+1, instructions, pState, pixels_visited, image, colour)
-	pixels_visited[(x, y+1)] = True
+	pState = checkDirection(x, y, 0, 1, instructions, pState, pixels_visited, image, colour)
+
 	
 	# # #
 	# o #
 	# # X
-	if(((x+1,y+1) not in pixels_visited) and image[x+1][y+1]==colour):
-		if(pState==1):
-			instructions.append((x,y))
-			instructions.append(0)
-			pState = 0
-		instructions.append((x+1, y+1))
-		pixels_visited[(x+1, y+1)] = True
-		pState = processLine(x+1, y+1, instructions, pState, pixels_visited, image, colour)
-	pixels_visited[(x+1, y+1)] = True
+	pState = checkDirection(x, y, 1, 1, instructions, pState, pixels_visited, image, colour)
+
 	
 	# # #
 	# o #
 	# X #
-	if(((x+1,y) not in pixels_visited) and image[x+1][y]==colour):
-		if(pState==1):
-			instructions.append((x,y))
-			instructions.append(0)
-			pState = 0
-		instructions.append((x+1, y))
-		pixels_visited[(x+1, y)] = True
-		pState = processLine(x+1, y, instructions, pState, pixels_visited, image, colour)
-	pixels_visited[(x+1, y)] = True
+	pState = checkDirection(x, y, 1, 0, instructions, pState, pixels_visited, image, colour)
+
 	
 	# # #
 	# o #
 #	X # #
-	if(((x+1,y-1) not in pixels_visited) and image[x+1][y-1]==colour):
-		if(pState==1):
-			instructions.append((x,y))
-			instructions.append(0)
-			pState = 0
-		instructions.append((x+1, y-1))
-		pixels_visited[(x+1, y-1)] = True
-		pState = processLine(x+1, y-1, instructions, pState, pixels_visited, image, colour)
-	pixels_visited[(x+1, y-1)] = True
+	pState = checkDirection(x, y, 1, -1, instructions, pState, pixels_visited, image, colour)
+
 	
 	# # #
 #	X o #
 	# # #
-	if(((x,y-1) not in pixels_visited) and image[x][y-1]==colour):
-		if(pState==1):
-			instructions.append((x,y))
-			instructions.append(0)
-			pState = 0
-		instructions.append((x, y-1))
-		pixels_visited[(x, y-1)] = True
-		pState = processLine(x, y-1, instructions, pState, pixels_visited, image, colour)
-	pixels_visited[(x, y-1)] = True
+	pState = checkDirection(x, y, 0, -1, instructions, pState, pixels_visited, image, colour)
+
 	
 #	X # #
 	# o #
 	# # #
-	if(((x-1,y-1) not in pixels_visited) and image[x-1][y-1]==colour):
-		if(pState==1):
-			instructions.append((x,y))
-			instructions.append(0)
-			pState = 0
-		instructions.append((x-1, y-1))
-		pixels_visited[(x-1, y-1)] = True
-		pState = processLine(x-1, y-1, instructions, pState, pixels_visited, image, colour)
-	pixels_visited[(x-1, y-1)] = True
+	pState = checkDirection(x, y, -1, -1, instructions, pState, pixels_visited, image, colour)
+
 		
 	if(instructions[len(instructions)-1] != 1):
 		instructions.append(1)
 	return 1
 	
+def checkDirection(x, y, x_direction, y_direction, instructions, pState, pixels_visited, image, colour):
+	if(((x + x_direction, y + y_direction) not in pixels_visited) and image[x + x_direction][y + y_direction]>=colour):
+		if(pState==1):
+			instructions.append((x,y))
+			instructions.append(0)
+			pState = 0
+		instructions.append((x + x_direction, y + y_direction))
+		pixels_visited[(x + x_direction, y + y_direction)] = True
+		pState = processLine(x + x_direction, y + y_direction, instructions, pState, pixels_visited, image, colour)
+	pixels_visited[(x + x_direction, y + y_direction)] = True
+	return pState
+	
 
-img = cv.imread('Testface.jpg',0)
+img = cv.imread('circle.jpg',0)
 edges = cv.Canny(img,100,200)
-
 
 
 print img
 print edges
-instructions = createInstructions(edges)
+instructions = createInstructions(img, 240)
 print "finish processing edges"
-#plt.plot([0.0,10.00], [0.0,10.0])
-#plt.show()
-plt.plot(instructions[0][0][0], instructions[0][2][0], instructions[0][0][1], instructions[0][2][1])
-plt.show()
+
 for instruction in instructions:
-	previous_point = -1
-	for point in instruction:
-		current_point = point
-		if(current_point==0):
-			continue
-		elif(current_point==1):
-			previous_point = -1
-		elif(previous_point == -1):
-			previous_point = point		
-		else:
-			print [previous_point[0], current_point[0]], [previous_point[1], current_point[1]]
-			line = plt.plot([previous_point[0], current_point[0]], [previous_point[1], current_point[1]])
-			plt.setp(line, color='r', linewidth=5.0)
+	print instruction
+# for instruction in instructions:
+# 	previous_point = -1
+# 	for point in instruction:
+# 		current_point = point
+# 		if(current_point==0):
+# 			continue
+# 		elif(current_point==1):
+# 			previous_point = -1
+# 		elif(previous_point == -1):
+# 			previous_point = point		
+# 		else:
+# 			print [previous_point[0], current_point[0]], [previous_point[1], current_point[1]]
+# 			line = plt.plot([previous_point[0], current_point[0]], [previous_point[1], current_point[1]])
+# 			plt.setp(line, color='r', linewidth=5.0)
 		
-plt.show()
+# plt.show()
